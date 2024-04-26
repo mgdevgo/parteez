0479b826bca5d52a877f34a5cd0f58ddfb50f86f
Max <2thousandmax@gmail.com>
2024-04-26 18:08:45 +0300

    fix: rename response to resource

--- pkg/shared/api/resource.go
+++ pkg/shared/api/resource.go
@@ -1,12 +1,13 @@
 package api
 
-type APIResponse struct {
-	Data []Data `json:"data"`
-}
-
-type Data struct {
+type Resource struct {
 	ID         string `json:"id"`
 	Type       string `json:"type"`
 	Href       string `json:"href"`
 	Attributes any    `json:"attributes"`
+	Relations  any    `json:"relations"`
+}
+
+type ResourceCollectionResponse struct {
+	Resources []Resource `json:"data"`
 }
